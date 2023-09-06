30cf97bb6b1b3b68950627c02a71cef0803c068c
darkpoetOX <ayindejam2009@yahoo.com>
2023-09-06 18:43:47 +0100

    form submit handler methods created

--- src/components/PetForm.js
+++ src/components/PetForm.js
@@ -1,7 +1,74 @@
+import { useState } from "react";
+
 const PetForm = () => {
 
+    const [statePet, setStatePet] = useState(   
+        {
+            name: "",
+            type: "",
+            breed: "",
+            age: 0
+        }
+    )
+
+    const handleChange = (event) => {       
+        let propertyName = event.target.name; // 'name' is the name of the target that triggered the event: 'name'/'type'
+        let copiedPet = {...statePet};
+        copiedPet[propertyName] = event.target.value;
+        setStatePet(copiedPet)
+    }
+
+    const handleFormSubmit = (event) => {  
+        event.preventDefault()
+        postPet(statePet)
+        setStatePet({
+            name: "",
+            type: "",
+            breed: "",
+            age: 0
+        })
+    }
+
     return (
-        <p>Hello from Pet Form</p>
+        <form onSubmit={handleFormSubmit}>
+            <h3>Add a new pet</h3>
+            
+            <input 
+                type="text"
+                placeholder="pet name"
+                onChange={handleChange}    
+                name="name"               
+                value={statePet.name} 
+            /> 
+                
+            <input
+                type="text"
+                placeholder="pet type" 
+                onChange={handleChange}    
+                name="type"               
+                value={statePet.type} 
+            />
+                
+            <input
+                type="text"
+                placeholder="pet breed"
+                onChange={handleChange}    
+                name="breed"               
+                value={statePet.breed}  
+            />
+
+            <input
+                type="number"
+                placeholder="pet age" 
+                onChange={handleChange}    
+                name="age"               
+                value={statePet.age} 
+            />
+        
+            
+            <button type="submit">OK</button>      
+        </form>
+
     );
 
 }
